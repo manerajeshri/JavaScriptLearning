2fcdbf6dd3d08112898f099b7191543a7334cee4
manerajeshri <manerajeshri@gmail.com>
2022-12-08 08:44:28 +0530

    java variable
    java variable and run

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;// variable declaration
+myName="Rajeshri Mane"; // variable initialization
+console.log(myName);
+// var x
+// x="123"
+// console.log(x);
+
+var myName1="R P Mane";
+console.log(myName1);
