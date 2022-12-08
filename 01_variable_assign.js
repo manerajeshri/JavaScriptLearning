fbd52fc32a8e01251757d2268e7fba0de486ef6f
manerajeshri <manerajeshri@gmail.com>
2022-12-08 10:03:41 +0530

    variable declaration and initialization in java
    variable declaration and initialization in java

---
+++ 01_variable_assign.js
@@ -0,0 +1,19 @@
+// Assignment no 1
+var myFullName;
+console.log(myFullName);
+myFullName="Rajeshri Mane";
+console.log(myFullName);
+myFullName="Rajeshri Prakash Mane";
+console.log(myFullName);
+
+var myLove="I Love only JS";
+console.log(myLove);
+
+var firstName="Rajeshri";
+var lastName="Mane";
+var age="32";
+var address="Pune";
+var pin_code="411033";
+var gender="Female";
+var is_married="Yes";
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
