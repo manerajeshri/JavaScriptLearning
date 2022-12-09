06f874c57058665292c10f6eefe5336b6de36f42
manerajeshri <manerajeshri@gmail.com>
2022-12-09 23:50:15 +0530

    variable and data types
    variable and data types using typeof operator

---
+++ 01_typeOf.js
@@ -0,0 +1,27 @@
+console.log('No 1--------------------------------------------');
+var bankName;
+console.log('Before Initilization :');
+console.log('1] Value :',bankName,'2] Data Type :',typeof bankName);
+bankName="SBI Bank";
+console.log('After Initilization  :');
+console.log('1] Value :',bankName,'2] Data Type :',typeof bankName);
+
+console.log('No 2--------------------------------------------');
+var marks='90%';
+console.log('1] Value :',marks,'2] Data Type :',typeof marks);
+
+console.log('No 3--------------------------------------------');
+var isWorking;
+console.log('Before Initilization :');
+console.log('1] Value :',isWorking,'2] Data Type :',typeof isWorking);
+isWorking=true;
+console.log('After Initilization :');
+console.log('1] Value :',isWorking,'2] Data Type :',typeof isWorking);
+
+console.log('No 4--------------------------------------------');
+var totalCount="Hundred and Seven";
+console.log('1] Value :',totalCount,'2] Data Type :',typeof totalCount);
+
+totalCount=107;
+console.log('After Upadate :');
+console.log('1] Value :',totalCount,'2] Data Type :',typeof totalCount);
