dbf896401164f4f6c51dbf0aef1afef8b1b51aca
manerajeshri <manerajeshri@gmail.com>
2022-12-10 23:34:30 +0530

    testing function
    testing function

---
+++ scratch.js
@@ -0,0 +1,16 @@
+function addTwoNo(v1,v2) {
+  var result=v1+v2;
+    // return result;
+    // console.log(result);
+}
+addTwoNo(1,2);
+
+function someName(param1,param2) {
+  
+    var a= param1+ 'Love'+ param2;
+    // console.log(a);
+    return a;
+    // console.log(a);
+}
+var b=someName('Me','You')
+console.log(b);
