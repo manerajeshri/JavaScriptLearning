696a9b9cce72182bae76ad20a02d5f7cb5014040
manerajeshri <manerajeshri@gmail.com>
2022-12-15 09:02:31 +0530

    ternary operator
    ternary operator

---
+++ 04_ternaryOperator.js
@@ -0,0 +1,10 @@
+
+// Trenary operator
+// if ssc mark greater than equal to 35 then pass else fail
+
+var sscMarks = 30;
+// test condition ? result1 : result2 ;
+var result = sscMarks>=35 ? "PASS" : "FAIL" ;
+
+console.log(`Result : ${result}`);
+
