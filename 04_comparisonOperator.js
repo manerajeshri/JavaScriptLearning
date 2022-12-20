08afe708256efd1a43247cc261b54b6d8145dad3
manerajeshri <manerajeshri@gmail.com>
2022-12-20 23:53:13 +0530

    comparison operator
    < > <= >=

--- 04_comparisonOperator.js
+++ 04_comparisonOperator.js
@@ -3,8 +3,8 @@ var num1 = 10;
 var num2 = 5;
 var num3 = 10
 var result = num1 > num2;
-console.log(`> Operator - Greater than ${result}`)
-console.log(`> Operator - Greater than ${num1>num3}`)
+console.log(`> Operator - Greater than : ${result}`)
+console.log(`> Operator - Greater than: ${num1>num3}`)
 
 var result = num1 < num2;
 console.log(`< Operator - Less than ${result}`)
