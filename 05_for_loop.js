09c0e2db420f508ea8f040eeb3950a2343998e3d
manerajeshri <manerajeshri@gmail.com>
2022-12-20 09:15:01 +0530

    for loop
    1] print no from 1 to 10
    2] print 2 to 20
    3] print 50 45 40....5

---
+++ 05_for_loop.js
@@ -0,0 +1,19 @@
+
+// print no from 1to 10
+console.log(`1] print no from 1to 10`);
+//   initilization;   condition ;  update    
+for (let index = 1;   index <= 10;  index = index+1) {
+    console.log(index);
+}
+
+// print 2 to 20
+console.log(`\n 2] print 2 to 20`);
+for (let index = 2; index <= 20; index = index+2) {
+    console.log(index);
+}
+
+// 50 45 40....5
+console.log(`\n 3] print 50 45 40....5`);
+for (let index = 50; index >= 5; index = index - 5) {
+    console.log(index);
+}
