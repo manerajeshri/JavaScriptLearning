18f9f736e04c6c41f6cfb308c64fabe3036db328
manerajeshri <manerajeshri@gmail.com>
2022-12-21 00:54:48 +0530

    for loop assignment
    1] print no from 5 to 15 incrementing by 1
    2] print numbers from 50 to 40 by decrementing by 1
    n3] first 15 ODD numbers
    4] first 10 Even numbers
    5] Table of 5
    6] table of 10
    7] Table of 10 in reverse 100--->10

--- 05_for_loop_assign.js
+++ 05_for_loop_assign.js
@@ -1,1 +1,34 @@
-console.log(`1] `);
+console.log(`for loop`);
+console.log(`\n1] print no from 5 to 15 incrementing by 1`);
+//  initialisation  condition    update
+for (let index = 5; index <= 15; index = index+1) {
+    console.log(`${index}`);
+}
+
+console.log(`\n2] print numbers from 50 to 40 by decrementing by 1` );
+for (let index = 50; index >= 40; index = index-1) {
+    console.log(`${index}`);
+}
+
+console.log(`\n3] first 15 ODD numbers`);
+for (let index = 1; index <= 30; index = index+2) {
+    console.log(`${index}`);
+}
+
+console.log(`\n4] first 10 Even numbers`);
+for (let index = 2; index <= 20; index = index+2) {
+    console.log(`${index}`);
+}
+
+console.log(`\n5] Table of 5`);
+for (let index = 5; index <= 50; index = index+5) {
+    console.log(`${index}`);
+}
+console.log(`\n6] table of 10`);
+for (let index = 10; index <= 100; index = index+10) {
+    console.log(`${index}`);
+}
+console.log(`\n7] Table of 10 in reverse 100--->10`);
+for (let index = 100; index >= 10; index = index-10) {
+    console.log(`${index}`);
+}
