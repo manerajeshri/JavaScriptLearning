0225e1ce6dcedb1e648ad55e2b96ba803a043672
manerajeshri <manerajeshri@gmail.com>
2022-12-20 23:53:32 +0530

    Nan
    operation on Nan

--- 04_Nan.js
+++ 04_Nan.js
@@ -9,9 +9,13 @@ var resulMul = resultDivide  * 10;
 console.log(`resulMul is : ${resulMul}`);
 
 var num1 = "100";
-console.log(`num1 is : ${num1}`);
-console.log(`typeof num1 is : ${typeof num1}`);
+console.log(`num1 is : ${num1}`); //100
+console.log(`typeof num1 is : ${typeof num1}`); //string
 
 var num = new Number("Prathamesh"); 
-console.log(`Prathamesh is: ${num}`);
-console.log(`typeof num is : ${typeof num1}`);
+console.log(`Prathamesh is: ${num}`); //Nan
+console.log(`typeof num is : ${typeof num}`); //Object
+
+var num1 = new Number("100"); 
+console.log(`Prathamesh is: ${num1}`); //100
+console.log(`typeof num is : ${typeof num1}`); //object
