1b502265680c4e0166b9f5aa2f7defadbeb778f3
manerajeshri <manerajeshri@gmail.com>
2022-12-20 23:56:21 +0530

    type conversion
    Explicit conversion
    converting string to number

--- 04_typeConversion.js
+++ 04_typeConversion.js
@@ -3,20 +3,21 @@ var numAdd = 10 + 40;
 var strAdd = "Raj " + 7 ;
 console.log(`strAdd is : ${strAdd}`);
 
-
+// Explicit conversion
 // converting string to number use + before string==>
+console.log(` + operator can do the the type conversion as well from string to number `);
 console.log(``);
 console.log(`converting string to number`);
 var numStr = "100" ;
 var num = + numStr;
-console.log(`type of "${numStr}" is: ${typeof numStr}`);
-console.log(`type of ${num} is: ${typeof num}`);
+console.log(`type of "${numStr}" is: ${typeof numStr}`);// type of "100" is: string
+console.log(`type of ${num} is: ${typeof num}`); // type of 100 is: number
 
 console.log(``);
 var numStr = "chandra" ;
 var num = + numStr;
-console.log(`type of "${numStr}" is: ${typeof numStr}`);
-console.log(`value is ${num} type of is: ${typeof num}`);
+console.log(`type of "${numStr}" is: ${typeof numStr}`); //type of "chandra" is: string
+console.log(`value is ${num} type of is: ${typeof num}`); // value is NaN type of is: number
 
 console.log(``);
 
@@ -24,41 +25,21 @@ console.log(``);
 
 var myNum=77;
 console.log(`My number is ${myNum} Datatype is ${typeof myNum}`);
+//My number is 77 Datatype is number
 
 var convToString = myNum.toString();
 console.log(`My number is ${convToString} Datatype is ${typeof convToString}`);
 
 
- // console.log(` + operator can do the the type conversion as well from string to number `);
-// var numStr = "100";
-// var num = + numStr;
-// console.log(`typeof numStr is: ${typeof numStr}`);
-// console.log(`typeof num is: ${typeof num}`);
-
-// var numStr = "Chandra";
-// var num = + numStr;
-// console.log(`typeof ${numStr} is: ${typeof numStr}`);
-// console.log(`typeof ${num} is: ${typeof num}`);
+var numAdd = 10 + 40; // 50
+var strAdd = "Pooja " + 70;
+ console.log(`"Pooja " + 70 is: ${strAdd}`);
 
 
-// var numAdd = 10 + 40; // 50
-// var strAdd = "Pooja " + 70;
-// console.log(`strAdd is: ${strAdd}`);
+ console.log(`\nImplicit conversion == comparison \n Is 20 == "20"? result :  ${20 == "20"}`); // Implicit conversion
 
-// console.log(` + operator can do the the type conversion as well from string to number `);
-// var numStr = "100";
-// var num = + numStr; // Explicit conversion
-// console.log(`typeof numStr is: ${typeof numStr}`);
-// console.log(`typeof num is: ${typeof num}`);
-
-// var numStr = "Chandra";
-// var num = + numStr;
-// console.log(`typeof ${numStr} is: ${typeof numStr}`);
-// console.log(`typeof ${num} is: ${typeof num}`);
-
-// console.log(`== comparison ${20 == "20"}`); // Implicit conversion
-
-// var myNumber = -77.333;
-// console.log(`myNumber values ${myNumber} and it's type is: ${typeof myNumber}`);
-// var strMyNumber = myNumber.toString();
-// console.log(`strMyNumber values ${strMyNumber} and it's type is: ${typeof strMyNumber}`);
+ // My number is 77 Datatype is string
+ var myNumber = -77.333;
+console.log(`\nmyNumber values ${myNumber} and it's type is: ${typeof myNumber}`);
+ var strMyNumber = myNumber.toString();
+ console.log(`strMyNumber values ${strMyNumber} and it's type is: ${typeof strMyNumber}`);
