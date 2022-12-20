9d0aa2da18abd269a5da79089308894408086477
manerajeshri <manerajeshri@gmail.com>
2022-12-20 09:20:03 +0530

    switch case
    Get the Day name by passing day number
    displaying 1- mon 2-Tue 3-wed 4-Thu 5-fri 6-sat 7 Sun

---
+++ 05_switch_case.js
@@ -0,0 +1,102 @@
+// displaying 1- mon 2-Tue 3-wed 4-Thu 5-fri 6-sat 7 Sun
+
+
+var dayNumber = 3;
+switch (dayNumber) {
+    case 1: console.log(`Today is Monday`);
+        break;
+        case 2: console.log(`Today is Tuesday`);
+        break;
+        case 3: console.log(`Today is Wedness `);
+        break;
+        case 4: console.log(`Today is Thursday`);
+        break;
+        case 5: console.log(`Today is Friday`);
+        break;
+        case 6: console.log(`Today is Saturday`);
+        break;
+        case 7: console.log(`Today is Sunday`);
+        break;
+    default: console.log(`Invalid day entered`);
+        break;
+}
+
+
+
+// Get the Day name by passing day number
+// 1 - Monday
+// 2 - Tuesday
+// 3 - Wednesday
+// 7 - Sunday
+
+// ================================ sir
+// var dayNumber = 9;
+// switch (dayNumber) {
+//   case 1:
+//     console.log(`Today is Monday`);
+//     console.log(`Monday is the start day of the week`);
+//     break;
+//   case 2:
+//     console.log(`Today is Tuesday`);
+//     break;
+//   case 3:
+//     console.log(`Today is Thursday`);
+//     break;
+//   case 4:
+//     console.log(`Today is Wednesday`);
+//     break;
+//   case 5:
+//     console.log(`Today is Friday`);
+//     break;
+//   case 6:
+//     console.log(`Today is Saturday`);
+//     break;
+//   case 7:
+//     console.log(`Today is Sunday`);
+//     console.log(`Sunday is the weekly off day`);
+//     break;
+//   default:
+//     console.log(`Not valid value provided`);
+//     break;
+// }
+
+// console.log(`After switch case statement`);
+
+
+// Get the Day name by passing day number
+// 1 - Monday
+// 2 - Tuesday
+// 3 - Wednesday
+// 7 - Sunday
+
+// var dayNumber = 9;
+// switch (dayNumber) {
+//   case 1:
+//     console.log(`Today is Monday`);
+//     console.log(`Monday is the start day of the week`);
+//     break;
+//   case 2:
+//     console.log(`Today is Tuesday`);
+//     break;
+//   case 3:
+//     console.log(`Today is Thursday`);
+//     break;
+//   case 4:
+//     console.log(`Today is Wednesday`);
+//     break;
+//   case 5:
+//     console.log(`Today is Friday`);
+//     break;
+//   case 6:
+//     console.log(`Today is Saturday`);
+//     break;
+//   case 7:
+//     console.log(`Today is Sunday`);
+//     console.log(`Sunday is the weekly off day`);
+//     break;
+//   default:
+//     console.log(`Not valid value provided`);
+//     break;
+// }
+
+// console.log(`After switch case statement`);
