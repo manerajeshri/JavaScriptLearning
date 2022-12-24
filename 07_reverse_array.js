e87c4f480702ae98c4378a628e0b9d0d1dbac245
manerajeshri <manerajeshri@gmail.com>
2022-12-24 19:56:37 +0530

    reversing array
    reversing array
    Even positioned value
    Odd positioned value

---
+++ 07_reverse_array.js
@@ -0,0 +1,22 @@
+
+let array =[4,6,7,8,3,2];
+console.log(`Given array is   : ${array}`); //Given array is   : 4,6,7,8,3,2
+
+let reversedArray = array.reverse();
+console.log(`Reverse array is : ${reversedArray}`);  //Reverse array is : 2,3,8,7,6,4
+
+var evenPosition = [], oddPosition =[];
+for (let index = 0; index < reversedArray.length; index++) {
+    
+    if (index%2==0) {
+        // console.log(reversedArray[index]);// 2 8 6 
+        evenPosition = evenPosition +" " + reversedArray[index] // concating o/p // 2 8 6
+        
+    }   
+    else {
+        oddPosition = oddPosition +" " + reversedArray[index] // concating o/p // 2 8 6
+    }
+    // console.log(evenPosition);
+}
+console.log(`Even positioned value : ${evenPosition}`); //Even positioned value :  2 8 6
+console.log(`Odd positioned value : ${oddPosition}`); //Odd positioned value :  3 7 4
