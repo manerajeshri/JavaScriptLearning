7002f401ce7b3216a20e7fe95591ad58dd9fc51c
manerajeshri <manerajeshri@gmail.com>
2022-12-26 11:14:26 +0530

    for of loop
    for of loop ===> iterator return values one by one

---
+++ 05_for_of_loop.js
@@ -0,0 +1,8 @@
+
+console.log(` for of loop ===> iterator return values one by one `);
+
+var arrayOfNum = [4,5,6,7,1,2,3,4,5,67];
+console.log(arrayOfNum);
+for (const element of arrayOfNum) {
+    console.log(element);
+}
