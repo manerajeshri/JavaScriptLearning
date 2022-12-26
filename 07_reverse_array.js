86ba58c9d2e8931136961a470db8b6a0a2180778
manerajeshri <manerajeshri@gmail.com>
2022-12-26 11:13:46 +0530

    reversing array
    .reverse
    using loop

--- 07_reverse_array.js
+++ 07_reverse_array.js
@@ -5,6 +5,15 @@ console.log(`Given array is   : ${array}`); //Given array is   : 4,6,7,8,3,2
 let reversedArray = array.reverse();
 console.log(`Reverse array is : ${reversedArray}`);  //Reverse array is : 2,3,8,7,6,4
 
+console.log(`Reversing array / traversing array using for loop in reverse order`);
+let lastIndex = array.length-1;
+for (let index = lastIndex; index >= 0 ; index--) {
+    const element = array[index];
+    // console.log(`**************\n Reversing array using loop :`);
+    console.log(element);
+    
+}
+
 var evenPosition = [], oddPosition =[];
 for (let index = 0; index < reversedArray.length; index++) {
     
