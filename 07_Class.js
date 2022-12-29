d4e7b03c8df0c506ef3719cb0fdc4700ca35f474
manerajeshri <manerajeshri@gmail.com>
2022-12-30 00:39:44 +0530

    class
    class
    constructor
    creating and calling function

---
+++ 07_Class.js
@@ -0,0 +1,50 @@
+const employeeAnil = {
+    name: "Anil",
+    id:11,
+    age:26,
+    city:"pune"
+}
+const employeeSunil = {
+    name: "Sunil",
+    id:22,
+    age:26,
+    city:"pune"
+}
+const employeeSmith = {
+    name: "Smith",
+    id:33,
+    age:26,
+    city:"pune"
+}
+const employeePoonam = {
+    name: "Poonam",
+    id:44,
+    age:26,
+    city:"pune"
+}
+// ================================ creating class
+class Employee{
+       constructor(name,id,age,city){
+        this.name = name;//this is used to point
+        this.id = id;
+        this.age = age;
+        this.city = city;
+       }
+        details(){
+            console.log(this.name, this.id, this.age, this.city)
+
+        }
+            
+    }
+    
+
+//new-used to create new object     className
+let empRaj = new Employee ("Raj",55,32,"satara");
+let empJay = new Employee ("Jay",66,34,"Delhi");
+let empAaru = new Employee ("Aaru",77,2,"pune");
+// console.log(empRaj);
+// console.log(empJay);
+// console.log(empAaru);
+empRaj.details();
+empJay.details();
+empAaru.details();
