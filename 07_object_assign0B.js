b88d3e92e4372e54c5e2d8676cebcdd12c02b79c
manerajeshri <manerajeshri@gmail.com>
2022-12-31 08:49:05 +0530

    object assign 0B
    object assign 0B

---
+++ 07_object_assign0B.js
@@ -0,0 +1,35 @@
+
+const sbiBank = {
+    bankName : " SBI Bank",
+    location : " Pune",
+    accountNo : "10000897 ",
+    ifsc : " SBI00009",
+    interestRate : "7% ",
+    showDetails : function () {
+        console.log(``);
+    }
+}
+
+const axisBank = {
+    bankName : " Axis Bank",
+    location : " Mumbai",
+    accountNo : " 00007654",
+    ifsc : " AXIS000056",
+    interestRate : "6.8% "
+}
+
+const hdfcBank = {
+    bankName : "HDFC Bank ",
+    location : " Sangli",
+    accountNo : "678543548 ",
+    ifsc : " HDFC000098",
+    interestRate : "6.5% "
+}
+
+const yesBank = {
+    bankName : "YES Bank ",
+    location : "Satara ",
+    accountNo : " 2345123",
+    ifsc : " YES000054",
+    interestRate : "5.9% "
+}
