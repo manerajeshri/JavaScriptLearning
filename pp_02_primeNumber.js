49e36c14ecd509df6fe7a2171384bddd52f9db07
manerajeshri <manerajeshri@gmail.com>
2023-01-01 20:57:15 +0530

    Prime Numbers
    Prime Numbers

---
+++ pp_02_primeNumber.js
@@ -0,0 +1,15 @@
+/*
+First Ten Prime Numbers
+
+The first ten primes are 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+
+Prime numbers are natural numbers that are divisible by only 1 and the number itself. 
+In other words, prime numbers are positive integers greater than 1 with exactly two factors, 1 and the number itself. 
+Some of the prime numbers include 2, 3, 5, 7, 11, 13, etc. Always remember that 1 is neither prime nor composite.
+Also, we can say that except for 1, the remaining numbers are classified as prime and composite numbers. 
+All prime numbers are odd numbers except 2, 2 is the smallest prime number and is the only even prime number.
+
+Prime numbers are the natural numbers greater than 1 with exactly two factors, i.e. 1 and the number itself.
+*/
+
+console.log(`Find the given number is prime or not :`);
