333c3c6a4ed83b65f7c9340d88a99a107ea2d66b
manerajeshri <manerajeshri@gmail.com>
2023-01-01 20:56:58 +0530

    Fibonacci Series
    Fibonacci Series

---
+++ pp_02_FibonacciSeries.js
@@ -0,0 +1,13 @@
+/* ref https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
+The Fibonacci numbers are the numbers in the following integer sequence.
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation
+Fn = Fn-1 + Fn-2 with seed values F0 = 0 and F1 = 1
+
+e.g. Write a function int fib(int n) that returns Fn. For example, if n = 0, then fib() should return 0. If n = 1, then it should return 1. For n > 1, it should return Fn-1 + Fn-2
+For n = 9 Output:34
+
+*/
+
+console.log(`Fibonacci Series till 7th occurance :`);
