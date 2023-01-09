ae7f519f8e0331b2de993ccf128850ec73154f0c
manerajeshri <manerajeshri@gmail.com>
2023-01-09 09:15:44 +0530

    11_arrowFunction.
    11_arrowFunction.

---
+++ 11_arrowFunction.js
@@ -0,0 +1,35 @@
+
+
+
+//  Arrow Function
+
+//  () => {
+//     console.log("Hello");
+// }
+
+// arrow function with No Agument and No return Value
+let show = () => {
+    console.log("Helllo ......arrow function with No Agument and No return Value");
+}
+
+show();
+
+//Arrow function with argument and NO return value
+console.log("\nArrow function with argument and NO return value");
+let sum =  (n1,n2) => {
+    console.log(n1+n2);
+}
+
+sum(1,3)
+
+// Arrow function with argument and return value
+console.log("\nArrow function with argument and return value");
+
+let multiply = (n1,n2) => {
+    return n1*n2
+}
+
+console.log(`Multiplication of 5 , 2 is : ${multiply(5,2)}`);
+
+
+
