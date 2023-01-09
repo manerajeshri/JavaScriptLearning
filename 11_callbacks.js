97e44322e42bd615c1435fb2375999799be3a41f
manerajeshri <manerajeshri@gmail.com>
2023-01-09 10:29:35 +0530

    11_callbacks
    11_callbacks

--- 11_callbacks.js
+++ 11_callbacks.js
@@ -1,10 +1,7 @@
-
-
 //Given home work
-
 // Anil -> Homework
-function anilHomework(callback) {
-    console.log("Anil is doing homework.");
+function anilHomework(rollNo , callback) { // I addede roll no
+    console.log(`My Roll no is ${ rollNo } and Anil is doing homework.`);
     console.log("After some time.. Anil did homework");
     callback();
 }
@@ -13,12 +10,25 @@ function sunilHomework() {
     console.log("I am Happy you have completed homework");
     console.log("Copying as it..");
 }
-anilHomework(sunilHomework);
+anilHomework(11,sunilHomework);
 // sunilHomework();
 
+// callback example 2
 
-function show() {
-    console.log("HI setTimeout");
+function add(n1,n2) {
+    console.log(`\nAddition is : ${n1+n2}`);
+}
+function mul(n1,n2) {
+    console.log(`Multiplication is : ${n1*n2}`);
 }
 
-setTimeout(show,3000);
+function addMul() {
+   console.log(`Callback==============================`);
+}
+
+addMul(add(1,3),mul(5,6))
+
+function show() {
+    console.log("\nHI setTimeout : inserts delay");
+}
+setTimeout(show,3000); // inserts delay
