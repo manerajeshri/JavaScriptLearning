e0ccb17be31398d361c1e5d3608c848f786d761a
manerajeshri <manerajeshri@gmail.com>
2023-01-15 23:50:28 +0530

    .forEach , includes .indexof join lastIndexOf
    .forEach , includes .indexof join lastIndexOf

--- practice_02_methods.js
+++ practice_02_methods.js
@@ -1,6 +1,6 @@
 
 
-const arrayOfNumbers = [ 1,2,3,4,5,6,7,8,9,10];
+const arrayOfNumbers = [ 1,2,3,4,5,6,7,8,9,10,2];
 console.log(arrayOfNumbers);
 console.log(arrayOfNumbers.length);
 console.log(arrayOfNumbers.concat(11,12,13));
@@ -10,3 +10,15 @@ const array2= [44,55,66,77];
 console.log(arrayOfNumbers.concat(array2));
 
 console.log(arrayOfNumbers.forEach((value,index) => console.log(index,"=>",value)));
+
+console.log(arrayOfNumbers.includes(9));
+console.log(arrayOfNumbers.includes(99));
+
+console.log(arrayOfNumbers.indexOf(4));
+console.log(arrayOfNumbers.join());
+console.log(arrayOfNumbers.join('*'));
+console.log(arrayOfNumbers.keys());
+
+arrayOfNumbers.keys()
+
+console.log(arrayOfNumbers.lastIndexOf(2)); // Returns the index of the last occurrence of a specified value in an array, or -1 if it is not present.
