eead395230f8036ca4789795d1a770181e2f6051
manerajeshri <manerajeshri@gmail.com>
2023-04-07 00:30:50 +0530

    revision Assignment 46: Arrow function
    revision Assignment 46: Arrow function

--- _revision.js
+++ _revision.js
@@ -1095,8 +1095,6 @@ for (let i = 0; i < arrayOfBanks.length; i++) {
     
 }
 
-console.clear();
-
 // Assignment 45: Set and Map hands on for objects
 // Create a class ‘Bank’ with all possible data members in such a way that all properties should
 // be initialized using constructor.
@@ -1163,3 +1161,103 @@ for (const i of bankArray) {
         console.log(`found  Kotak bank ==> ${i.bank_name} ${i.ifsc}`);
     }
 }
+
+// Assignment 46: Arrow function
+// Write a arrow function such that - Pls don’t forget to log result on console with meaningful msg
+// 1. With no args and no return value, log message on console inside arrow function
+// a. “Good Morning, Today is Monday”
+let show= ()=>{
+    console.log(`“Good Morning, Today is Monday”`);
+}
+show();
+// 2. With 3 args and no return value, for received 3 parameters perform the multiplication
+// a. Values to be passed ⇒ 5, 5, 2
+let mul = (n1,n2,n3=1)=>{
+console.log(`Multiplication is `, n1*n2*n3);
+}
+mul(5,5,2)
+// b. Invoke the same function for values ⇒ 10, 4 [Note: assign default value to 3rd arg as 1 ]
+mul(10,4)
+// 3. With 5 args and return value such as, for received params it should do the addition
+let add5 = (n1,n2,n3,n4,n5)=>{
+return console.log(`Addition = `, n1+n2+n3+n4+n5);
+}
+// a. Values to be passed ⇒ 100, 100, 200, 349, 756
+add5(100, 100, 200, 349, 756)
+// b. Log the returned result on console with meaningful message for both step 3.a and 3.c
+// c. Invoke the same arrow function for values: “I am”, “learning”, “ ES6”, ‘ features’, “ in depth”
+add5("I am", " learning", " ES6", " features", " in depth")
+
+console.clear();
+// Assignment 47
+// Note: Please create the Employee class as it →
+// - While creating objects pass the values as it is →
+// - Add all the created emp objects inside array namely  as ‘array_employess’
+// Solve the below problem statements as  & log result on console with meaningful msg
+
+class Employee {
+    constructor(emp_id, emp_name, emp_dept, emp_salary, emp_company) { 
+        this.emp_id = emp_id;    
+        this.emp_name = emp_name;
+        this.emp_dept = emp_dept;
+        this.emp_salary= emp_salary;
+        this.emp_company= emp_company;
+    }
+}
+    const emp_anil = new Employee (22, "Anil", "IT", 50000, "TCS"); 
+    const emp_radha = new Employee(33, "Radha", "HR", 74000, "Wipro"); 
+    const emp_rishi = new Employee(55, "Rishi", "Finance", 47000, "TCS"); 
+    const emp_sonali = new Employee (66, "Sonali", "Finance", 45000, "Infy"); 
+    const emp_monika = new Employee(77, "Monika", "IT", 40000, "Wipro"); 
+    const emp_viny = new Employee (88, "Vinayak", "IT", 75000, "TCS"); 
+    const enp_mahi = new Employee(99, "Mahesh", "HR", 85000, "Infy");
+
+arrayOfEmp = [emp_anil,emp_radha,emp_rishi,emp_sonali,emp_monika,emp_viny,enp_mahi];// creating array of object
+
+// 1. Find the all the employees working in ‘TCS’ and log only employee names and company name on console
+
+for (const i of arrayOfEmp) {
+ if (i.emp_company === "TCS") {
+    console.log(`employee names ${i.emp_name} and company name ${i.emp_company}`);
+ }   
+}
+
+// 2. Find the ‘Finance’ department employees, log only department and employee name on console
+for (const i of arrayOfEmp) {
+    if (i.emp_dept === "Finance") {
+        console.log(`department ${i.emp_dept} and employee name ${i.emp_name}`);
+    }
+}
+
+// 3. Find the employees whose name starts with ‘R’ and complete employee details on console [ hint –> startsWith(R“) ]
+for (const i of arrayOfEmp) {
+    if (i.emp_name.startsWith("R") ) {
+        console.log(`department ${i.emp_dept} and employee name ${i.emp_name}`);
+    }
+}
+
+// 4. Find the employees whose salary is greater than 70000, and emp name, company and salary on console
+for (const i of arrayOfEmp) {
+    if (i.emp_salary >=70000 ) {
+        console.log(`company ${i.emp_company}  salary ${i.emp_salary} and employee name ${i.emp_name}`);
+    }
+}
+
+// 5. Find the emp’s whose salary greater than or equal 50000 and from ‘IT’ department, log complete emp details on console
+for (const i of arrayOfEmp) {
+    if (i.emp_salary >=50000 && i.emp_dept === "IT" ) {
+        console.log(`company ${i.emp_company} department ${i.emp_dept} salary ${i.emp_salary} and employee name ${i.emp_name}`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
